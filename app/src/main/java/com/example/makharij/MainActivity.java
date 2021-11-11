b9ff4716d4f858c71a003e47d25219bab54cf3aa
Ali <>
2021-11-11 20:36:50 +0500

    all done

--- app/src/main/java/com/example/makharij/MainActivity.java
+++ app/src/main/java/com/example/makharij/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.makharij;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -15,13 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView score_view;
-
-        Intent intent = getIntent();
+        //TextView score_view;
+        Button score_view;
         score_view=findViewById(R.id.score_view);
+        //score_view.setVisibility(View.GONE);
+
+
+        score_view.setText("Score will appear here");
+        Intent intent = getIntent();
         score_view.setText(intent.getStringExtra("score"));
+        score_view.setBackgroundColor(Color.RED);
+        //score_view.setVisibility(View.VISIBLE);
+        score_view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "My score in Makharij quiz is: " + intent.getStringExtra("score"));
+                sendIntent.setType("text/plain");
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
 
-
+            }
+        });
         Button button = findViewById(R.id.rep);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -32,9 +48,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-//
-//// this has prob
         Button lrn =findViewById(R.id.learn);
         lrn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -51,13 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
-
-
 
     }
 
--- app/src/main/java/com/example/makharij/quiz.java
+++ app/src/main/java/com/example/makharij/quiz.java
@@ -7,10 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
 
@@ -19,10 +16,8 @@ public class quiz extends AppCompatActivity {
 
     String[] quest={"أ ہ","ع ح","غ خ","ق","ک" ,"ض" ,"ل" ,"ن" ,"ر" ,"ت د ط","ن"};
     String[] ans={"End of Throat","Middle of Throat","Start of the Throat","Base of Tongue which is near Uvula touching the mouth roof","Portion of Tongue near its base touching the roof of mouth", "One side of the tongue touching the molar teeth","Rounded tip of the tongue touching the base of the frontal 8 teeth","Rounded tip of the tongue touching the base of the frontal 6 teeth","Rounded tip of the tongue and some portion near it touching the base of the frontal 4 teeth" ,"Tip of the tongue touching the base of the front 2 teeth"};
-    //String[] option={"End of Throat","Middle of Throat","Start of the Throat","Base of Tongue which is near Uvula touching the mouth roof"};
     TextView t1,score_quiz;
     Button next;
-    //RadioButton r1,r2,r3,r4;
     Button r1,r2,r3,r4;
     int score=0 , i=0 ;
 
@@ -44,7 +39,7 @@ public class quiz extends AppCompatActivity {
                        score_quiz.setText("your Score is :"+score);
 
                        Intent intent = new Intent(quiz.this, MainActivity.class);
-                       intent.putExtra("score", "your Score is :"+score);
+                       intent.putExtra("score", "Share your Score :"+score);
                        startActivity(intent);
 
 
@@ -62,12 +57,9 @@ public class quiz extends AppCompatActivity {
 
                    }else if (i==9)
                    {
-                    /*   score_quiz=findViewById(R.id.question);
-                       score_quiz.setText("your Score is :"+score);*/
-                      // Log.d("score:","your Score is :"+score );
 
                        Intent intent = new Intent(quiz.this, MainActivity.class);
-                       intent.putExtra("score", "your Score is :"+score);
+                       intent.putExtra("score", "Share your Score :"+score);
                        startActivity(intent);
 
                    }
@@ -85,10 +77,8 @@ public class quiz extends AppCompatActivity {
 
                    }else if (i==9)
                    {
-                      /* score_quiz=findViewById(R.id.textView);
-                       score_quiz.setText("your Score is :"+score);*/
                        Intent intent = new Intent(quiz.this, MainActivity.class);
-                       intent.putExtra("score", "your Score is :"+score);
+                       intent.putExtra("score", "Share your Score :"+score);
                        startActivity(intent);
 
                    }
@@ -99,7 +89,7 @@ public class quiz extends AppCompatActivity {
            });
 
 
-           r4 = findViewById(R.id.r4);
+           r4 = findViewById(R.id.next);
            r4.setOnClickListener(new View.OnClickListener() {
                public void onClick(View v) {
                    if (i<9 && r1.getText().toString()!=ans[i] && r2.getText().toString()!=ans[i] && r3.getText().toString()!=ans[i]) {
@@ -107,13 +97,9 @@ public class quiz extends AppCompatActivity {
 
                    }else if (i==9)
                    {
-                      /* score_quiz=findViewById(R.id.textView);
-                       score_quiz.setText("your Score is :"+score);*/
                        Intent intent = new Intent(quiz.this, MainActivity.class);
-                       intent.putExtra("score", "your Score is :"+score);
+                       intent.putExtra("score", "Share your Score :"+score);
                        startActivity(intent);
-
-
                    }
                    i++;
                    generate_question();
@@ -181,7 +167,7 @@ public class quiz extends AppCompatActivity {
             r1 = findViewById(R.id.r1);
             r2 = findViewById(R.id.r2);
             r3 = findViewById(R.id.r3);
-            r4 = findViewById(R.id.r4);
+            r4 = findViewById(R.id.next);
             r4.setText("None of above");
             int ansL = ans.length;
             Random r = new Random();
@@ -197,111 +183,3 @@ public class quiz extends AppCompatActivity {
 
 }
 
-
-
-/*
-
-
-
-
-
-
-
-
-public class quiz extends AppCompatActivity {
-
-    String[] quest={"أ ہ","ع ح","غ خ","ق","ک" ,"ض" ,"ل" ,"ن" ,"ر" ,"ت د ط"};
-    String[] ans={"End of Throat","Middle of Throat","Start of the Throat","Base of Tongue which is near Uvula touching the mouth roof","Portion of Tongue near its base touching the roof of mouth", "One side of the tongue touching the molar teeth","Rounded tip of the tongue touching the base of the frontal 8 teeth","Rounded tip of the tongue touching the base of the frontal 6 teeth","Rounded tip of the tongue and some portion near it touching the base of the frontal 4 teeth" ,"Tip of the tongue touching the base of the front 2 teeth"};
-
-   //String[] option={"End of Throat","Middle of Throat","Start of the Throat","Base of Tongue which is near Uvula touching the mouth roof"};
-    TextView t1;
-    Button next;
-    RadioButton r1,r2,r3,r4,rb ;
-
-    RadioGroup rg;
-
-    int score=0 , i=0, quest_count=0;;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
-
-    while(quest_count<10){
-        rg=findViewById(R.id.radioGroup);
-        t1=findViewById(R.id.question);
-        r1=findViewById(R.id.r1);
-        r2=findViewById(R.id.r2);
-        r3=findViewById(R.id.r3);
-        r4=findViewById(R.id.r4);
-        r4.setText("None of above");
-        int ansL= ans.length;
-        Random r = new Random();
-        int i1 = r.nextInt(ansL-0)+0;
-        t1.setText(quest[i]);
-        r1.setText(ans[i1]);
-        i1 = r.nextInt(ansL - 0) + 0;
-        r2.setText(ans[i1]);
-        i1 = r.nextInt(ansL - 0) + 0;
-        r3.setText(ans[i1]);
-
-        int radioid= rg.getCheckedRadioButtonId();
-        rb=findViewById(radioid);
-        if(r1.isChecked() || r2.isChecked() || r3.isChecked() || r4.isChecked() ) {
-            RadioButton rb = findViewById(rg.getCheckedRadioButtonId());
-            String check_ans = rb.getText().toString();
-            if (rb.getText() == ans[i]) {
-                score++;
-            }
-        }
-        next = findViewById(R.id.next);
-        next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                quest_count++;
-                i++;
-            }
-        });
-
-}
-        */
-/*Boolean RadioButtonState2 = r2.isChecked();
-        Boolean RadioButtonState3 = r3.isChecked();
-        Boolean RadioButtonState4 = r4.isChecked();*//*
-
-        */
-/*if(r1.isChecked() && r1.getText() == ans[i]){
-            score++;
-        }else if (r2.isChecked() && r2.getText() == ans[i])
-        {
-            score++;
-        }else if (r3.isChecked() && r3.getText() == ans[i])
-        {
-            score++;
-        }else if (r4.isChecked()){
-            score++;
-        }else{
-
-        }*//*
-
-
-    }
-
-    */
-/*public void checkButton(View v){
-        int radii= rg.getCheckedRadioButtonId();
-        rb=findViewById(radii);
-        *//*
-*/
-/*RadioButton rb = findViewById(rg.getCheckedRadioButtonId());
-        String check_ans = rb.getText().toString();*//*
-*/
-/*
-        if(rb.getText()==ans[i]){
-            score++;
-        }
-
-
-    }
-*//*
-
-}*/
